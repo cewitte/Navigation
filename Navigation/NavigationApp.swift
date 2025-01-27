c7faf6f6390e70d7530677bfe9edb2e31cfc9b0b
Carlos Eduardo Witte <cewitte@icloud.com>
2025-01-27 08:35:53 -0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Carlos Eduardo Witte on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
